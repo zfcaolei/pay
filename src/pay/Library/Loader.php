b8f9155d5a0cfd0f4fbd81486cc4b63d767dd4a5
cl <455549675@qq.com>
2019-07-10 10:10:15 +0800

    更新加载第三方sdk

--- src/pay/Library/Loader.php
+++ src/pay/Library/Loader.php
@@ -1,12 +1,13 @@
 <?php
 namespace pay\Library;
-require_once('../alipay/aop/AopClient.php');
+
 
 class Loader{
 
     public function index()
     {
-        var_dump(new \AopClient());
+        $a  = new \AopClient();
+        var_dump($a);
     }
 
 
